a40d504a5ccc8ff53b5d2ad3228054275072f098
Vlad Gheorghe <vlad.gheorghe@gmail.com>
2018-02-10 16:14:18 +0200

    change to public fields

--- src/main/python/domain.py
+++ src/main/python/domain.py
@@ -3,12 +3,10 @@ Created on Feb 10, 2018
 
 @author: vlad
 '''
-from samba.dcerpc.epmapper import EPMAPPER_STATUS_NO_MORE_ENTRIES
 
 class Issue:
     '''
     '''
-
     def __init__(self, key, summary):
         self._key=key
         self._summary=summary
@@ -27,24 +25,16 @@ class StructureNode:
 
 class SvnLog:
     def __init__(self):
-        self._entries=[]
-
-    def entries(self):
-        self._entries
+        self.entries=[]
         
     def add(self, entry):
-        self._entries.append(entry)
+        self.entries.append(entry)
 
 
 class SvnLogEntry:
     def __init__(self, revision, comment, paths, jiraId ):
-        self._revision=revision
-        self._comment=comment
-        self._paths=paths
-        self._jiraId=jiraId
+        self.revision=revision
+        self.comment=comment
+        self.paths=paths
+        self.jiraId=jiraId
         
-    def revision(self): self._revision
-    def comment(self): self._comment
-    def paths(self): self._paths
-    def jiraId(self): self._jiraId
-
